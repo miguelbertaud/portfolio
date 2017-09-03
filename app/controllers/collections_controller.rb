5ec1218ff9f765c2b52d6c74f74949744cda2992
Miguel Bertaud <miguel.bertaud@gmail.com>
2017-09-02 23:18:33 -0500

    Integrated new and create actions for portfolios

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -2,4 +2,22 @@ class CollectionsController < ApplicationController
   def index
     @portfolio_items = Collection.all
   end
+
+  def new
+    @portfolio_item = Collection.new
+  end
+
+  def create
+    @portfolio_item = Collection.new(params.require(:collection).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @@portfolio_item.save
+        format.html { redirect_to collections_path, notice: 'Your portfolio item is now live.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+
 end
