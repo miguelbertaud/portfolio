885e7f145a76d38497b79a4fb56cf14a95518847
Miguel Bertaud <miguel.bertaud@gmail.com>
2017-09-04 23:11:55 -0500

    Implemented custom action for updating statuses for blog posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   resources :collections, except: [:show]
-  get 'collection/:id', to: 'collections#show', as: 'collection_show' 
+  get 'collection/:id', to: 'collections#show', as: 'collection_show'
   root to: 'pages#home'
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
-  resources :blogs
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
