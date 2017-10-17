e7180b0be3b528acb1839a03dd19880b8792d8a0
Miguel Bertaud <miguel.bertaud@gmail.com>
2017-10-16 23:03:34 -0500

    integrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+
+  def copyright_generator
+    BertaudViewTool::Renderer.copyright 'Miguel Bertaud', 'All rights reserved'
+  end
 end
